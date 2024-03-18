530522569fc83b28c8aa0828ba304f582ae40dac
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-18 14:03:30 +0500

    authRouter

--- src/db/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const mongoUri =
-  "mongodb+srv://oracle:773311889922@nodeexpressproject.kxoaosu.mongodb.net/track-server?retryWrites=true&w=majority&appName=NodeExpressProject";
-
-mongoose
-  .connect(mongoUri)
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8080;
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+
+app.use(authRoutes);
+
+const mongoUri =
+  "mongodb+srv://oracle:773311889922@nodeexpressproject.kxoaosu.mongodb.net/track-server?retryWrites=true&w=majority&appName=NodeExpressProject";
+
+mongoose
+  .connect(mongoUri)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hi there");
