3d4154c8af1e27250b1f6fa300073fb0e4d92e66
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-18 16:07:25 +0500

    validation signup

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
-  res.send("You made a Post Request");
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.send("You made a Post Request");
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
